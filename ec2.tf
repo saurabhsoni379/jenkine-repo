e09235dafd759f6576c2ac2536198e5cd5f16a9c
saurabh soni <saurabh@gkmit.co>
2024-08-11 23:15:48 +0530

    another ec2 added

--- ec2.tf
+++ ec2.tf
@@ -9,7 +9,21 @@ resource "aws_instance" "ec2-server" {
   subnet_id = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   tags = {
-    Name = "jenkins-server"
+    Name = "jenkins-server-2"
+  }
+  key_name = aws_key_pair.ss_key.key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+}
+
+
+resource "aws_instance" "ec2-server-2" {
+  ami = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+  subnet_id = module.vpc.public_subnets[1]
+  associate_public_ip_address = true
+  tags = {
+    Name = "jenkins-server-2"
   }
   key_name = aws_key_pair.ss_key.key_name
   vpc_security_group_ids = [aws_security_group.sg.id]
