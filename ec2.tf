de6c0c287c63a2d221d006ade21a3faa611bcc23
saurabhsoni379 <saurabh@gkmit.co>
2024-08-11 23:08:49 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "ss_key" {
-  key_name = "jenkins-key"
+  key_name = "jenkins-key-1"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEvr/v/PlFRzwoMIc2wP5CZAZQU7L+3H6bt1iVA+BluR mac@Macs-MacBook-Pro.local"
 }
 
